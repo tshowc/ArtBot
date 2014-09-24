43b7804ee783d3f454d58b318a9baf8c71470644
Terena Chao <terenaschao@yahoo.com>
2014-09-24 19:15:16 -0400

    Added files

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-  consumer_key:         'JUQBbJSV2hVK5SDeHrgNGc9PG',
-  consumer_secret:      'VPKrs1gsLZ7ebAK0HJIwCET6GTFFDIipBYHoxOvqG6XXuPFY1a',
-  access_token:         '2805787254-AGvK7EGBBxp52c2kdk859Ct64nZOQCYa6X6L0BD',
-  access_token_secret:  '8OzTq9wdITkfyeSmOUNBTNXrjhBnV4JXpGKEpgQZxQZWB'
+  consumer_key:         ENV['APIKEY'],
+  consumer_secret:      ENV['APISecret'],
+  access_token:         ENV['AccessTok'],
+  access_token_secret:  ENV['AccessTokSec']
 };
