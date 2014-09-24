b2c5823a18b99a073871ceea52a047a7eb50878c
Terena Chao <terenaschao@yahoo.com>
2014-09-24 19:52:21 -0400

    Removed

--- config.js
+++
@@ -1,7 +0,0 @@
-// Put your own Twitter App keys here. See README.md for more detail.
-module.exports = {
-  consumer_key:         ENV['APIKEY'],
-  consumer_secret:      ENV['APISecret'],
-  access_token:         ENV['AccessTok'],
-  access_token_secret:  ENV['AccessTokSec']
-};
