0619738456249b3555a52bc565dc4addf8fecbb4
Terena Chao <terenaschao@yahoo.com>
2014-09-24 18:08:24 -0400

    Add new files

---
+++ infinite.js
@@ -0,0 +1,3 @@
+while(0!=1){
+	console.log("the possibilities are infinite.");
+	}
---
+++ test.js
@@ -0,0 +1,79 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var Twit = require('twit');
+var T = new Twit(require('./config.js'));
+
+function tweet(){
+	var linePoem = [];
+	var url = 'http://poems.com/poem.php?date=';
+	var randNum;
+	do{
+	//console.log("Reassigning");
+	randNum = Math.floor((Math.random() * 16333) | 16000);
+	}while(randNum > 16333); 
+	console.log(randNum);
+	request (url + randNum, function(error, response, body){
+	if(!error && response.statusCode === 200){
+		var $ = cheerio.load(body);
+		var poem = $('#poem').text();
+		var arrPoem = poem.split("\n");
+		var i = 0;
+		while ( i < 10){
+			if (arrPoem[i].indexOf(" ") == 0)
+			{
+				console.log("SKIP");
+				linePoem[i] = arrPoem[i+1];
+				i++;
+			}
+			else{
+			
+				linePoem[i] = arrPoem[i];
+			//	console.log(arrPoem[i]);
+	//			console.log(linePoem[i]);
+				i++;
+			}
+		}
+	//	console.log('Source:', url + randNum);
+		//console.log(poem);
+		linePoem = linePoem[4] + "\n" + linePoem[5] +"\n\n" + "Source: " + url + randNum;
+
+	//	console.log("+++++++++++++++++");
+		console.log(linePoem);
+
+	if (linePoem.length > 140){
+		console.log("Retweeting");
+		tweet();
+	}
+	else{
+		
+	T.post( 'statuses/update', { status: linePoem } , function(err, reply) {
+		if (err) {
+			console.log('error', err);
+			var check = 0;
+			for (check = 0; check < 2; check++){
+			console.log("Checking");
+			tweet();}
+		}
+		else {
+			console.log('reply:', reply);
+		}
+	});
+	}
+	}
+	});
+}
+
+tweet();
+
+
+setInterval(function () {
+	try{
+		tweet();
+	}
+	catch (e) {
+		console.log(e);
+	}
+}, 60000 * 5);
+
+
+
