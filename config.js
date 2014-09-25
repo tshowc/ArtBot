838ce05d347d6b3bba10c3bb0f3b569df0aaba5b
Terena Chao <terenaschao@yahoo.com>
2014-09-24 20:45:10 -0400

    Changed

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-  consumer_key:         process.env.APIKey,
-  consumer_secret:      process.env.APISecret,
-  access_token:         process.env.AccessTok,
-  access_token_secret:  process.env.AccessTokSec
+  consumer_key:         'process.env.APIKey',
+  consumer_secret:      'process.env.APISecret',
+  access_token:         'process.env.AccessTok',
+  access_token_secret:  'process.env.AccessTokSec'
 };
